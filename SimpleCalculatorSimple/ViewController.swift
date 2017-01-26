686f0450b7b60b9db01bb4c51634cec9964578e6
James Browning <jbxmv@mail.missouri.edu>
2017-01-25 20:12:20 -0500

    Programs works, but getting confusing and complicated.

--- SimpleCalculatorSimple/ViewController.swift
+++ SimpleCalculatorSimple/ViewController.swift
@@ -10,6 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var globalLabel : String = ""
+    var plusIsPressed : Bool = false
+    var equalsIsPressed : Bool = false
+    var buttonPressed : Bool = false
+
+    var sum : Int = 0
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if !equalsIsPressed {
+            buttonPressed = true
+            if let title = sender.currentTitle {
+                globalLabel.append(title)
+                label.text = globalLabel
+                plusIsPressed = false
+            }
+        } else {
+            globalLabel = ""
+            label.text = globalLabel
+        }
+    }
+    
+    @IBAction func plus(_ sender: UIButton) {
+        equalsIsPressed = false
+        if !plusIsPressed {
+            globalLabel = ""
+            plusIsPressed = true
+        }
+        
+        if let fromLabel = label.text {
+            if buttonPressed {
+                print(fromLabel)
+                sum += Int(fromLabel)!
+                label.text = "\(sum)"
+                buttonPressed = false
+                globalLabel = ""
+            }
+        }
+    }
+    
+    @IBAction func equals(_ sender: UIButton) {
+        if !equalsIsPressed {
+            if let fromLabel = label.text {
+                if !plusIsPressed {
+                    sum += Int(fromLabel)!
+                    label.text = "\(sum)"
+                    globalLabel = ""
+                }
+            }
+            equalsIsPressed = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
