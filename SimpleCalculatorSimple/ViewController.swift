887a71604988f0f362c668c2b71eef1e725f3e42
James Browning <jbrowni8@Jamess-MacBook-Pro.local>
2017-01-30 19:42:41 -0500

    first commit

--- SimpleCalculatorSimple/ViewController.swift
+++ SimpleCalculatorSimple/ViewController.swift
@@ -12,55 +12,29 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
     var globalLabel : String = ""
-    var plusIsPressed : Bool = false
-    var equalsIsPressed : Bool = false
-    var buttonPressed : Bool = false
-
     var sum : Int = 0
     
     @IBAction func buttonPressed(_ sender: UIButton) {
-        if !equalsIsPressed {
-            buttonPressed = true
-            if let title = sender.currentTitle {
-                globalLabel.append(title)
-                label.text = globalLabel
-                plusIsPressed = false
-            }
-        } else {
-            globalLabel = ""
+        if let title = sender.currentTitle {
+            globalLabel.append(title)
             label.text = globalLabel
         }
     }
     
     @IBAction func plus(_ sender: UIButton) {
-        equalsIsPressed = false
-        if !plusIsPressed {
+        if let firstHalf = label.text {
             globalLabel = ""
-            plusIsPressed = true
-        }
-        
-        if let fromLabel = label.text {
-            if buttonPressed {
-                print(fromLabel)
-                sum += Int(fromLabel)!
-                label.text = "\(sum)"
-                buttonPressed = false
-                globalLabel = ""
-            }
+            sum += Int(firstHalf)!
         }
     }
     
     @IBAction func equals(_ sender: UIButton) {
-        if !equalsIsPressed {
-            if let fromLabel = label.text {
-                if !plusIsPressed {
-                    sum += Int(fromLabel)!
-                    label.text = "\(sum)"
-                    globalLabel = ""
-                }
-            }
-            equalsIsPressed = true
+        if let secondHalf = label.text {
+            globalLabel = ""
+            sum += Int(secondHalf)!
         }
+        label.text = "\(sum)"
+        sum = 0
     }
     
     override func viewDidLoad() {
